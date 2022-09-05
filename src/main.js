adda0e06328de6dd0d4f67158124dbfcf7d8a3bb
DeddyS <deddy@privyid.tech>
2022-09-06 04:36:06 +0700

    take out test

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import App from "./App.vue";
 // import "./assets/main.css";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
-import "./assets/site.css"
+import "./assets/site.css";
 import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 
 Vue.use(BootstrapVue);
